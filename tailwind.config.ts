6c2bb45d4a6bd388f3f4fc42077912d2c9d394b7
Alicio Alves <alicio.alvesm@gmail.com>
2024-09-01 16:40:33 -0300

    feat: add design system config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        main: '#4E2096',
+        primary: '#26203B',
+        secondary: '#4D4E4E',
+        stroke: '#DFE4E8',
+        placeholder: '#B2B2B2',
+        danger: '#E03131',
+        success: '#08D58B'
       },
+
+      fontFamily: {
+        inter: ["Inter", "sans-serif"]
+      }
     },
   },
   plugins: [],
