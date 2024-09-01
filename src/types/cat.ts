6c49000aa745b0da55f1d54f25ed5ee1f50486eb
Alicio Alves <alicio.alvesm@gmail.com>
2024-09-01 15:29:49 -0300

    chore: add cat type interface

---
+++ src/types/cat.ts
@@ -0,0 +1,11 @@
+export interface Cat {
+  id: number;
+  name: string;
+  age: number;
+  breed: string;
+  description?: string;
+  image?: string;
+  adopted: boolean;
+  created_at: string;
+  updated_at: string;
+}
